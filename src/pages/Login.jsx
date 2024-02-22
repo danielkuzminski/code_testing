b3ff675cb36ef6e2865f52d4c041b5dc48200ebb
Daniel Kuzminski <dankuzmina@gmail.com>
2024-02-22 02:30:53 +0100

    login and signin functionality completed

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,17 @@
-import { Link } from 'react-router-dom'
+//styles
 import './Login.css'
+//router
+import { Link } from 'react-router-dom'
+//react
 import { useState } from 'react'
-
+//hooks
+import { useLogin } from '../hooks/UseLogin'
 
 export default function Login() {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+
+  const {error, pending, login} = useLogin()
 
   const resetForm = () => {
     setEmail('')
@@ -14,16 +20,22 @@ export default function Login() {
 
   const handleSubmit = (e) => {
     e.preventDefault()
+
+    login(email, password)
+
+    resetForm()
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-        <h1>Login</h1>
+    <form className='signup' onSubmit={handleSubmit}>
+        <h1 className='signup__title'>Login</h1>
+        {error && <span>{error}</span>}
         <label>
           <span>email</span>
           <input 
             type="email"
             required
+            placeholder='email...'
             onChange={(e) => {
               setEmail(e.target.value)
             }}
@@ -35,15 +47,16 @@ export default function Login() {
           <input 
             type="password"
             required
+            placeholder='password...'
             onChange={(e) => {
               setPassword(e.target.value)
             }}
             value={password}
             />
         </label>
-        <p>Not a member yet?</p>
-        <Link to='/signup'>Signup</Link>
-        <button type="submit">Submit</button>
+        <button className='btn-submit' type="submit">Submit</button>
+        <p className='signup__advice'>Not a member yet?</p>
+        <Link to='/signup' className='btn-signup'>Signup</Link>
     </form>
   )
 }
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,9 +1,73 @@
+//style
 import './Signup.css'
+//react
+import { useState } from 'react'
+//hooks
+import {useSignup} from '../hooks/useSignup'
 
 export default function Signup() {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [displayName, setDisplayName] = useState('')
+
+    const {error, pending, signup} = useSignup()
+
+    const resetForm = () => {
+        setEmail('')
+        setPassword('')
+        setDisplayName('')
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        signup(email, password, displayName)
+
+        resetForm()
+    }
+
   return (
-    <form>
-        <h1>Signup</h1>
+    <form className='signup' onSubmit={handleSubmit}>
+        <h1 className='signup__title'>Signup</h1>
+        {error && <p>{error}</p>}
+        <label>
+            <span>email</span>
+            <input 
+                type="email" 
+                required 
+                placeholder='your email...'
+                onChange={(e) => {
+                    setEmail(e.target.value)
+                }}
+                value={email}
+            />
+        </label>
+        <label>
+            <span>password</span>
+            <input 
+                type="password"
+                required
+                placeholder='your password...'
+                onChange={(e) => {
+                    setPassword(e.target.value)
+                }}
+                value={password}
+            />
+        </label>
+        <label>
+            <span>user name</span>
+            <input 
+                type="text"
+                required
+                placeholder='set your user name...'
+                onChange={(e) => {
+                    setDisplayName(e.target.value)
+                }}
+                value={displayName}
+            />
+        </label>
+        {!pending && <button className='btn-submit'>Submit</button>}
+        {pending && <button className='btn-submit'>Sending</button>}
     </form>
   )
 }
