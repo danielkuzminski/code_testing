631bb34f6f7135c82146ed22bea1fa14af294d79
Daniel Kuzminski <dankuzmina@gmail.com>
2024-03-04 14:29:06 +0100

    navbar interactions

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,25 +1,45 @@
 //styles
-import './Navbar.css'
+import "./Navbar.css"
 
 //hooks
-import {useLogout} from '../hooks/useLogout'
+import { useLogout } from "../hooks/useLogout"
 
-import { NavLink } from 'react-router-dom'
+//router
+import { NavLink } from "react-router-dom"
+
+//assets
+import ps4 from "../assets/ps4.png"
 
 export default function Navbar() {
-  const {logout} = useLogout()
+	const { logout } = useLogout()
 
-  return (
-    <nav className='navigation'>
-        <p className="title">code battleground</p>
-        <ul className='nav-items'>
-            <li className='nav-item'><NavLink to='/'>Home</NavLink></li>
-            <li className='nav-item'><NavLink to='/games'>Games</NavLink></li>
-            <li className='nav-item'><NavLink to='/add_game'>Add Games</NavLink></li>
-            <li className='nav-item'><NavLink>to be added</NavLink></li>
-            <li className='nav-item'><NavLink to='/login'>Login</NavLink></li>
-            <li className='nav-item' onClick={logout}><NavLink>logout</NavLink></li>
-        </ul>
-    </nav>
-  )
+
+	return (
+		<nav className="navigation">
+			<p className="title">
+				<img className="logo" src={ps4} />
+				Szuflada geeka
+			</p>
+			<ul className='nav-items'>
+				<li className="nav-item">
+					<NavLink to="/">Home</NavLink>
+				</li>
+				<li className="nav-item">
+					<NavLink to="/games">Gry</NavLink>
+				</li>
+				<li className="nav-item">
+					<NavLink>Komiksy</NavLink>
+				</li>
+				<li className="nav-item">
+					<NavLink to="/add_game">Dodaj</NavLink>
+				</li>
+				<li className="nav-item">
+					<NavLink to="/login">Login</NavLink>
+				</li>
+				<li className="nav-item" onClick={logout}>
+					<NavLink>logout</NavLink>
+				</li>
+			</ul>
+		</nav>
+	)
 }
